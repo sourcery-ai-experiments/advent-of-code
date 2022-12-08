fd8bb9a000edf42b7872dfbafc501868debab15d
Bill <bill@thewallisfamily.co.uk>
2022-12-08 09:08:58 +0000

    Add template files for day 8

---
+++ advent_of_code/day_8/oop.py
@@ -0,0 +1,12 @@
+"""
+OOP solution for day 8.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 8 problem!
+    """
+
+    return [0, 0]
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main() -> None:
 
     advent_of_code.solutions.print_all_solutions(
         print_all=False,
-        use_sample=False,
+        use_sample=True,
         profile_solutions=False,
         repeat=10_000,
     )
