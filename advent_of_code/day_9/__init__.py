8e1b43ccf2ba865d281fb25f073d9fb3b3fa7ea8
Bill <bill@thewallisfamily.co.uk>
2022-12-09 18:58:45 +0000

    Add details to day 9 files

--- advent_of_code/day_9/__init__.py
+++ advent_of_code/day_9/__init__.py
@@ -1,5 +1,5 @@
 """
-Day 9: 
+Day 9: Rope Bridge
 
 https://adventofcode.com/2022/day/9/input
 """
@@ -8,4 +8,12 @@ from advent_of_code.day_9.optimal import solution as solution_optimal
 
 
 SAMPLE_INPUT = """
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
 """
