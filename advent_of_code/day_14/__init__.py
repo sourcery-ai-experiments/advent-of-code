bf3f7a29c786d51a9c3e337dc069b4dac5d8a7fd
Bill <bill@thewallisfamily.co.uk>
2022-12-14 09:09:20 +0000

    Add template files for day 14

---
+++ advent_of_code/day_14/__init__.py
@@ -0,0 +1,13 @@
+"""
+Day 14: Regolith Reservoir
+
+https://adventofcode.com/2022/day/14/input
+"""
+from advent_of_code.day_14.oop import solution as solution_oop
+from advent_of_code.day_14.optimal import solution as solution_optimal
+
+
+SAMPLE_INPUT = """
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+"""
