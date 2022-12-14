f68d2c1392872d8ac791490c2f123a6b5e7020ed
Bill <bill@thewallisfamily.co.uk>
2022-12-14 09:02:43 +0000

    Add solution for part 2 of day 9

--- advent_of_code/day_9/oop.py
+++ advent_of_code/day_9/oop.py
@@ -3,14 +3,12 @@ OOP solution for day 9.
 """
 from __future__ import annotations
 
-import enum
 from typing import Any
 
 
-# noinspection DuplicatedCode
 class Position(tuple):
     """
-    A position on a ZxZ plane.
+    A position on a 2-dimensional plane of integers.
     """
     def __new__(cls, *args):
         return super(Position, cls).__new__(cls, args)
@@ -63,6 +61,116 @@ class Position(tuple):
         return cls(*iter(tuple_))
 
 
+class Knot:
+    """
+    A knot, which only has a position and its history.
+    """
+    def __init__(self, position: Position):
+        self._position = position
+        self.position_history = [position]
+
+    def __str__(self):
+        return str(self.position)
+
+    def __repr__(self):
+        return str(self.position)
+
+    @property
+    def position(self) -> Position:
+        """
+        The knot's position.
+        """
+        return self._position
+
+    @position.setter
+    def position(self, value: Position) -> None:
+        """
+        Set the knot's position.
+        """
+        self._position = value
+        self.position_history.append(value)
+
+
+class Rope:
+    """
+    A rope, whose head has a position and whose tail has a position.
+    """
+    def __init__(self, starting_position: Position, knots: int):
+        """
+        Create a rope with a starting position and some number of knots.
+        """
+        self.knots = knots
+        self.knot_positions: dict[int, Knot] = {
+            i: Knot(starting_position)
+            for i in range(knots)
+        }
+
+    def __str__(self):
+        return f"head={self.head_knot}, tail={self.tail_knot}"
+
+    @property
+    def head_knot(self) -> Knot:
+        """
+        The head's position.
+        """
+        return self.knot_positions[0]
+
+    @property
+    def tail_knot(self) -> Knot:
+        """
+        The tail's position.
+        """
+        return self.knot_positions[self.knots - 1]
+
+    def resolve_knot_positions(self) -> None:
+        """
+        Update the knot positions to correctly follow the head around.
+        """
+        for key, knot in self.knot_positions.items():
+            if key != 0:  # head
+                self.knot_positions[key].position = resolve_knot_position(
+                    upper_knot=self.knot_positions[key - 1].position,
+                    lower_knot=knot.position,
+                )
+
+    def move(self, direction: str) -> None:
+        """
+        Move the head of the rope, and subsequently the tail of the rope if
+        necessary.
+        """
+        self.knot_positions[0].position += DIRECTIONS[direction]
+        self.resolve_knot_positions()
+
+    def follow_instructions(self, instructions: list[str]) -> None:
+        """
+        Follow the instructions given, which are to move in different directions
+        by various amounts.
+        """
+        for instruction in instructions:
+            direction, quantity = instruction.split()
+            for _ in range(int(quantity)):
+                self.move(direction)
+
+
+def resolve_knot_position(upper_knot: Position, lower_knot: Position) -> Position:
+    """
+    Update the lower knot's position to correctly follow the upper knot
+    around.
+    """
+    x, y = (upper_knot - lower_knot)
+    x_, y_ = abs(x), abs(y)
+    if x_ == 2 and y_ == 2:
+        return lower_knot + Position(x // x_, y // y_)
+    elif x_ == 2:
+        return lower_knot + Position(x // x_, y)
+    elif y_ == 2:
+        return lower_knot + Position(x, y // y_)
+    elif x_ > 2 or y_ > 2:
+        raise ValueError(f"Larger distance than expected, found {x, y=}")
+    else:
+        return lower_knot
+
+
 # Math co-ordinates
 UP = Position(0, 1)
 DOWN = Position(0, -1)
@@ -76,93 +184,6 @@ DIRECTIONS = {
 }
 
 
-class Rope:
-    """
-    A rope, whose head has a position and whose tail has a position.
-    """
-    def __init__(self, starting_position: Position, knots: int):
-        """
-        Create a rope with a starting position and some number of knots.
-        """
-        self._head_position = starting_position
-        self._tail_position = starting_position
-
-        self.knots = knots
-        self.knot_positions: dict[int, Position] = {
-            i: starting_position
-            for i in range(knots)
-        }
-
-        self.head_position_history = [starting_position]
-        self.tail_position_history = [starting_position]
-
-    def __str__(self):
-        return f"head={self.head_position}, tail={self.tail_position}"
-
-    @property
-    def head_position(self) -> Position:
-        """
-        The head's position.
-        """
-        return self._head_position
-
-    @head_position.setter
-    def head_position(self, value: Position) -> None:
-        """
-        Set the head's position.
-        """
-        self._head_position = value
-        self.head_position_history.append(value)
-
-    @property
-    def tail_position(self) -> Position:
-        """
-        The tail's position.
-        """
-        return self._tail_position
-
-    @tail_position.setter
-    def tail_position(self, value: Position) -> None:
-        """
-        Set the tail's position.
-        """
-        self._tail_position = value
-        self.tail_position_history.append(value)
-
-    def resolve_tail_position(self) -> None:
-        """
-        Update the tail position to correctly follow the head around.
-
-        TODO: This depends on the number of knots in a different way. Need to
-        resolve them in order.
-        """
-        x, y = (self.head_position - self.tail_position)
-        if abs(x) == 2:
-            self.tail_position += Position(x / 2, y)
-        elif abs(y) == self.knots:
-            self.tail_position += Position(x, y / 2)
-        else:
-            ValueError(f"Larger distance than expected, found {x, y=}")
-
-    def move(self, direction: str) -> None:
-        """
-        Move the head of the rope, and subsequently the tail of the rope if
-        necessary.
-        """
-        self.head_position += DIRECTIONS[direction]
-        self.resolve_tail_position()
-
-    def follow_instructions(self, instructions: list[str]) -> None:
-        """
-        Follow the instructions given, which are to move in different directions
-        by various amounts.
-        """
-        for instruction in instructions:
-            direction, quantity = instruction.split()
-            for _ in range(int(quantity)):
-                self.move(direction)
-
-
 def solution(input_: str) -> list[Any]:
     """
     Solve the day 9 problem!
@@ -173,6 +194,6 @@ def solution(input_: str) -> list[Any]:
     rope_2.follow_instructions(instructions=input_.strip().split("\n"))
 
     return [
-        len(set(rope_1.tail_position_history)),
-        len(set(rope_2.tail_position_history)),
+        len(set(rope_1.tail_knot.position_history)),
+        len(set(rope_2.tail_knot.position_history)),
     ]
