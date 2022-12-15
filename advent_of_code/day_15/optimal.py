a8e2cb384a26601d07d881e2fbf4b49ca4d205cd
Bill <bill@thewallisfamily.co.uk>
2022-12-15 07:23:37 +0000

    Add template for day 15

---
+++ advent_of_code/day_15/optimal.py
@@ -0,0 +1,12 @@
+"""
+Optimal (?) solution for day 15.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 15 problem!
+    """
+
+    return [0, 0]
