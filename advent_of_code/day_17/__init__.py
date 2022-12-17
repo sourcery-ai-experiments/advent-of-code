4ba4c45e305225c6665aab141b402ed35508fc19
Bill <bill@thewallisfamily.co.uk>
2022-12-17 08:58:47 +0000

    Add template files for day 17

---
+++ advent_of_code/day_17/__init__.py
@@ -0,0 +1,12 @@
+"""
+Day 17: Pyroclastic Flow
+
+https://adventofcode.com/2022/day/17/input
+"""
+from advent_of_code.day_17.oop import solution as solution_oop
+from advent_of_code.day_17.optimal import solution as solution_optimal
+
+
+SAMPLE_INPUT = """
+>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
+"""
---
+++ advent_of_code/day_17/oop.py
@@ -0,0 +1,12 @@
+"""
+OOP solution for day 17.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 17 problem!
+    """
+
+    return [0, 0]
