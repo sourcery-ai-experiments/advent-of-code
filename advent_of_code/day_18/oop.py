c80099218225d7ee773f135815e9fe5d506ba12b
Bill <bill@thewallisfamily.co.uk>
2022-12-21 13:40:13 +0000

    Add template files for days 18 through 21

---
+++ advent_of_code/day_18/oop.py
@@ -0,0 +1,12 @@
+"""
+OOP solution for day 18.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 18 problem!
+    """
+
+    return [0, 0]
---
+++ advent_of_code/day_19/optimal.py
@@ -0,0 +1,12 @@
+"""
+Optimal (?) solution for day 19.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 19 problem!
+    """
+
+    return [0, 0]
