08c6affc25f5747ad8baafd89aba9a084df3fb21
Bill <bill@thewallisfamily.co.uk>
2022-12-22 21:16:29 +0000

    Add template files for day 22

---
+++ advent_of_code/day_22/__init__.py
@@ -0,0 +1,25 @@
+"""
+Day 22: Monkey Map
+
+https://adventofcode.com/2022/day/22/input
+"""
+from advent_of_code.day_22.oop import solution as solution_oop
+from advent_of_code.day_22.optimal import solution as solution_optimal
+
+
+SAMPLE_INPUT = """
+        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5
+"""
---
+++ advent_of_code/day_22/optimal.py
@@ -0,0 +1,12 @@
+"""
+Optimal (?) solution for day 22.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 22 problem!
+    """
+
+    return [0, 0]
