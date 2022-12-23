b457d0dbd2a5a0d4c56bbd16288db14bce402528
Bill <bill@thewallisfamily.co.uk>
2022-12-23 19:13:18 +0000

    Add template files for day 23

---
+++ advent_of_code/day_23/__init__.py
@@ -0,0 +1,18 @@
+"""
+Day 23: Unstable Diffusion
+
+https://adventofcode.com/2022/day/23/input
+"""
+from advent_of_code.day_23.oop import solution as solution_oop
+from advent_of_code.day_23.optimal import solution as solution_optimal
+
+
+SAMPLE_INPUT = """
+....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..
+"""
---
+++ advent_of_code/day_23/optimal.py
@@ -0,0 +1,12 @@
+"""
+Optimal (?) solution for day 23.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 23 problem!
+    """
+
+    return [0, 0]
