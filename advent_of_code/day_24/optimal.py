988df39254d6c067f27c0c8ad66f487439196492
Bill <bill@thewallisfamily.co.uk>
2022-12-24 16:03:37 +0000

    Add template files for day 24

---
+++ advent_of_code/day_24/optimal.py
@@ -0,0 +1,12 @@
+"""
+Optimal (?) solution for day 24.
+"""
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 24 problem!
+    """
+
+    return [0, 0]
