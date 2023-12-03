f40f9c5dabb8802c40b43c61ccd93c61fdc9a6e6
Bill <bill@thewallisfamily.co.uk>
2023-12-03 19:27:27 +0000

    🍱 feat: Standardise the daily solution function

--- advent_of_code/utils/daily_files.py
+++ advent_of_code/utils/daily_files.py
@@ -99,6 +99,18 @@ class FileCreator:
                 from __future__ import annotations
 
                 import logging
+                from typing import Any
+
+                def solution(input_: str) -> list[Any]:
+                    """
+                    Solve the day {self.day} problem!
+                    """
+                    logging.basicConfig(level="DEBUG")
+
+                    return [
+                        0,
+                        0,
+                    ]
                 '''
             ),
         )
