cdf91c00e44f6e35bb7fca7776c18c995a76d0ee
Bill <bill@thewallisfamily.co.uk>
2023-12-03 19:51:08 +0000

    ✅ test: Add placeholder modules for unit tests

---
+++ tests/test__solutions.py
@@ -0,0 +1,4 @@
+"""
+Tests for the ``advent_of_code/solutions.py`` module.
+"""
+import advent_of_code.solutions as solutions
---
+++ tests/utils/test__daily_files.py
@@ -0,0 +1,20 @@
+"""
+Tests for the ``advent_of_code/utils/daily_files.py`` module.
+"""
+import pathlib
+
+import pytest
+
+import advent_of_code.utils.daily_files as daily_files
+from advent_of_code.constants import ROOT
+
+
+@pytest.fixture
+def root(tmp_path: pathlib.Path) -> pathlib.Path:
+    """
+    Return a temporary directory.
+    """
+    global ROOT  # Dirty hack, but I don't know how to do it better
+    ROOT = tmp_path
+
+    return ROOT
