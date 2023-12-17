a0ca568b89ebbfef85de90e3fb7490ba91209a78
Bill <bill@thewallisfamily.co.uk>
2023-12-17 09:48:47 +0000

    ✨ feat: Add files for days 15, 16, and 17 (2023)

---
+++ advent_of_code/year_2023/day_15/__init__.py
@@ -0,0 +1,6 @@
+"""
+Day 15: Lens Library
+
+https://adventofcode.com/2023/day/15/input
+"""
+from advent_of_code.year_2023.day_15.main import solution
---
+++ advent_of_code/year_2023/day_17/main.py
@@ -0,0 +1,19 @@
+"""
+Solution for day 17.
+"""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+
+def solution(input_: str) -> list[Any]:
+    """
+    Solve the day 17 problem!
+    """
+    logging.basicConfig(level="DEBUG")
+
+    return [
+        0,
+        0,
+    ]
